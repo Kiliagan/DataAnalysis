e40303887ecef48ed3e48f4abc4e4580a7d5bed2
Kiliagan <kiliansullivan26@gmail.com>
2018-04-17 23:03:18 +0100

    convert data from csv to array using different methods

--- LinearData.py
+++ LinearData.py
@@ -1,10 +1,16 @@
 #LinearData.py by Kiliagan
 
 def readFile(file):
+	xPos = []
+	yPos = []
 	with open(file) as f:
 		for row in f:
-			print row
-	
+			holder = row.replace('\n','').split(',')
+			xPos.append(holder[0])
+			yPos.extend([holder[1]])
+	return xPos, yPos
 if __name__ == "__main__":
 	file = 'Cordinates.csv'
-	readFile(file)
+	xPos,yPos = readFile(file)
+	print xPos
+	print yPos
